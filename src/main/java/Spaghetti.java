6fa2a06dd4ba2cc34fa0729e0238bb46181a3223
loll <notEmail@tt.com>
2025-01-30 14:32:16 +0200

    fix

--- src/main/java/Spaghetti.java
+++ src/main/java/Spaghetti.java
@@ -1,19 +1,33 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
-import org.openqa.selenium.manager.SeleniumManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
 
 public class Spaghetti {
 
     public static void main(String[] args){
-        EdgeOptions options=new EdgeOptions();
+        ChromeOptions options=new ChromeOptions();
 //        options.addArguments("--headless");
         options.addArguments("--start-maximized");
-        WebDriver driver= new EdgeDriver(options);
-        driver.get("https://www.google.com");
+
+        WebDriver driver = new ChromeDriver(options);
+//        Wait wait=new WebDriverWait(driver, Duration.ofSeconds(4));
+//        Wait wait=new  FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(2)).withMessage("testing")..pollingEvery(Duration.ofSeconds(2));
+
+        driver.navigate().to("https://www.google.com");
+        // implicit wait
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+
         By search=By.xpath("//textarea[@title='بحث']");
+        /* id - name - link text -partial link text - xpath - css selector
+        */
+
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(search));   //explicit wait
+
 
         By searchButton=test(1);
         driver.findElement(search).sendKeys("test");
